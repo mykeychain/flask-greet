b99e977fe9d275f4e416d0960ea2a38f8c9a4a9f
Mike Chang <mchang95@gmail.com>
2021-07-07 11:35:15 -0700

    adds greet app

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/welcome')
+def welcome():
+    """ Returns simple welcome greeting. """
+    return "<html><body><h1>welcome</h1></body></html>"
+
+@app.route('/welcome/<location>')
+def welcome_location(location):
+    """ Returns welcome greeting to specific location. """
+    return f"<html><body><h1>welcome {location}</h1></body></html>"
