706d4f9c96926a7b53fc257325b40f548e6cede4
Mike Chang <mchang95@gmail.com>
2021-07-07 13:28:18 -0700

    adds new line after docstrings

--- app.py
+++ app.py
@@ -5,9 +5,11 @@ app = Flask(__name__)
 @app.route('/welcome')
 def welcome():
     """ Returns simple welcome greeting. """
+
     return "<html><body><h1>welcome</h1></body></html>"
 
 @app.route('/welcome/<location>')
 def welcome_location(location):
     """ Returns welcome greeting to specific location. """
+    
     return f"<html><body><h1>welcome {location}</h1></body></html>"
